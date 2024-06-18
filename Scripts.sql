ed390647c1eb7027fe6043e19887fbec071d6f08
M Rafay Randhawa <75304976+RafayRandhawa@users.noreply.github.com>
2024-06-18 18:29:54 +0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -5,7 +5,7 @@ USE `onlinedeliverysystem`;
 -- Host: 127.0.0.1    Database: onlinedeliverysystem
 -- ------------------------------------------------------
 -- Server version	8.0.33
-
+ 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
